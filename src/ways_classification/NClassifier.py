c982a5fd85ca0c30bfd4ece31c39785cfbc92bdb
Viraj Mehta <thebigmehtaphor@gmail.com>
2016-05-11 21:14:30 -0700

    Changed NClassifier to separate classifiers for each way

--- src/ways_classification/NClassifier.py
+++ src/ways_classification/NClassifier.py
@@ -3,14 +3,14 @@ import LinearClassifier
 
 class NClassifier(object):
     """Calls classification methods of the N classifiers we have"""
-    def __init__(self, list_of_classifier_names, data_model):
+    def __init__(self, classifier_type, data_model):
         super(NClassifier, self).__init__()
         self.list_of_classifiers = []
-        for name in list_of_classifier_names:
-            if name == "Naive Bayes":
+        for way in data_model.get_list_of_ways():
+            if classifier_type == "Naive Bayes":
                 pass
-            elif name == "Linear":
-                self.list_of_classifiers.append(LinearClassifier(data_model))
+            elif classifier_type == "Linear":
+                self.list_of_classifiers.append(LinearClassifier(data_model, way))
         
 
     def train(self):
@@ -18,14 +18,14 @@ class NClassifier(object):
         training_errors = []
         for classifier in self.list_of_classifiers:
             training_errors.append(classifier.train())
-        return training_errors
+        return sum(training_errors) / len(training_errors)
 
     def test(self):
     """Calls the test method of each classifer and returns an array of the test errors"""
         test_errors = []
         for classifer in self.list_of_classifiers:
             test_errors.append(classifier.test())
-        return test_errors
+        return sum(test_errors) / len(test_errors)
 
     def classify(self, description):
     """Takes a single course description and returns an array of the training labels"""
