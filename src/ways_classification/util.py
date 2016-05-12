3e8c126aee3430788a27d6371faacff54f2a24d1
Marcus Vincent Gomez <mvgomez@stanford.edu>
2016-05-11 21:00:06 -0700

    added convert_to_matrix in util

--- src/ways_classification/util.py
+++ src/ways_classification/util.py
@@ -1,11 +1,15 @@
 import numpy as np
 
 def convert_to_matrix(data_list):
-    strings = [x[0] for x in data_list]
-    strings = strings.join(" ")
-    strings = strings.strip()
-    l_strings = strings.split()
-    s_strings = set(l_strings)
-    V = len(s_strings)
-    mat = np.asmatrix(np.zeros((len(data_list,V))))
-    
+    list_of_words = [word for word in x[0] for x in data_list]
+    tmp = dict(enumerate(list_of_words))
+    pos = dict((y,x) for x,y in tmp.iteritems())
+    V = len(set(list_of_words))
+    X = zeros((len(data_list),V))
+    y = zeros((len(data_list),1))
+    for i in range(len(data_list)):
+        point = data_list[i]
+        y[i] = point[1]
+        for word in point[0]:
+             X[i,pos[word]] = X[i,pos[word]] + 1
+    return (X,y)
