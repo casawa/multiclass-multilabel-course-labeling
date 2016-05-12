23f53a9321ce1d1eb549802ea255a265c83b4374
Viraj Mehta <thebigmehtaphor@gmail.com>
2016-05-11 21:04:54 -0700

    Now passes data model to classifier init

--- src/ways_classification/NClassifier.py
+++ src/ways_classification/NClassifier.py
@@ -6,26 +6,25 @@ class NClassifier(object):
     def __init__(self, list_of_classifier_names, data_model):
         super(NClassifier, self).__init__()
         self.list_of_classifiers = []
-        self.data_model = data_model
         for name in list_of_classifier_names:
             if name == "Naive Bayes":
                 pass
             elif name == "Linear":
-                self.list_of_classifiers.append(LinearClassifier())
+                self.list_of_classifiers.append(LinearClassifier(data_model))
         
 
     def train(self):
     """Calls the train method of each classifer and returns an array of the training errors"""
         training_errors = []
         for classifier in self.list_of_classifiers:
-            training_errors.append(classifier.train(self.data_model.train_data))
+            training_errors.append(classifier.train())
         return training_errors
 
     def test(self):
     """Calls the test method of each classifer and returns an array of the test errors"""
         test_errors = []
         for classifer in self.list_of_classifiers:
-            test_errors.append(classifier.test(self.data_model.test_data))
+            test_errors.append(classifier.test())
         return test_errors
 
     def classify(self, description):
