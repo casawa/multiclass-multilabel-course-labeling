b7858c925d0cceab93f5e63a6a1c0f80a94f8f78
Viraj Mehta <thebigmehtaphor@gmail.com>
2016-05-20 09:55:59 -0700

    wrote test harness

---
+++ TestHarness.py
@@ -0,0 +1,28 @@
+from Classifier import Classifier
+import LinearClassifier as lc
+import NaiveBayes as nb
+import DataModel as dm
+
+def TestLinear():
+    '''This is a wrapper around the test harness for the linear classifier.'''
+    data = dm.DataModel()
+    list_of_ways = data.get_list_of_ways()
+    for way in list_of_ways:
+        clf = lc.LinearClassifier(data, way)
+        _test(clf, way)
+
+
+def TestNB():
+    '''This is a wrapper aroung the test harness for the Naive Bayes classifier'''
+    data = dm.DataModel()
+    list_of_ways = data.get_list_of_ways()
+    for way in list_of_ways:
+        clf = nb.NaiveBayes(data, way)
+        _test(clf, way)
+  
+def _test(classifier, way):
+    '''The test harness. Takes a classifer and prints statistics'''
+    print way
+    print 'Training error: {}'.format(classifier.train())
+    print 'Test error: {}'.format(classifier.test())
+
